ec9c035a55a536c1b474dc0c9386cd5a3063c223
Neharika Rout <neharikarout01@gmail.com>
2024-07-27 20:37:52 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,12 @@ const store = new MongoDBStore({
 
 store.on("error", (err) => console.log(err));
 
+app.use(function(req,res,next) {
+	res.header("Access-Control-Allow-Origin","*")
+	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
+
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,9 +11,7 @@ const client = new ApolloClient({
 	uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql", // the URL of our GraphQL server.
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
 	credentials: "include",
-	fetchOptions:{
-		mode:'no-cors'
-	} // This tells Apollo Client to send cookies along with every request to the server.
+ // This tells Apollo Client to send cookies along with every request to the server.
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
