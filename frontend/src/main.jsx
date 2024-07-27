e909c026ce6f5cfd646e6b60c7782d3015ea8f6b
Neharika Rout <neharikarout01@gmail.com>
2024-07-27 20:02:53 +0530

    updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,10 @@ const client = new ApolloClient({
 	// TODO => Update the uri on production
 	uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql", // the URL of our GraphQL server.
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
-	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+	credentials: "include",
+	fetchOptions:{
+		mode:'no-cors'
+	} // This tells Apollo Client to send cookies along with every request to the server.
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
