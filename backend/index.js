5461d0d9c5de4e8479f336557bbf01db060a7124
Neharika Rout <neharikarout01@gmail.com>
2024-07-27 21:02:17 +0530

    edited index.js

--- backend/index.js
+++ backend/index.js
@@ -39,11 +39,7 @@ const store = new MongoDBStore({
 
 store.on("error", (err) => console.log(err));
 
-app.use(function(req,res,next) {
-	res.header("Access-Control-Allow-Origin","*")
-	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-	next();
-})
+
 
 app.use(
 	session({
